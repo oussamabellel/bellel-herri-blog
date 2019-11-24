c159f3bcd168e4db8bf38969f97a4378cc9ec75d
UNitech <bellel.oussama@live.fr>
2019-11-24 19:50:31 +0100

    index

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class TestController extends AbstractController
 {
     /**
-     * @Route("/test", name="test")
+     * @Route("/", name="test")
      */
     public function index()
     {
