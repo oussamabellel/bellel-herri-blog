2536a22cfc30d9fe0454cd4ac2b24af2beceb88b
Bellel Oussama <oussama.bellel@etu.umontpellier.fr>
2019-11-26 19:40:13 +0100

    add + modify + delete posts

---
+++ src/Controller/AdminPostController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use App\Form\PostType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminPostController extends AbstractController
+{
+
+    private $repository;
+
+    public function __construct(PostRepository $repository) {
+
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route("/admin/posts", name="admin_post")
+     */
+    public function index()
+    {
+        $posts = $this->repository->findAll();
+        return $this->render('admin_post/index.html.twig', [
+            'controller_name' => 'AdminPostController',
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/admin/posts/edit/{id}", name="admin_edit")
+     */
+    public function edit (Post $post,Request $request) 
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+           $em = $this->getDoctrine()->getManager();
+           $em->flush();
+           $this->addFlash('success', 'Post edited sucessfully');
+           return $this->redirectToRoute('admin_post');
+        }
+        return $this->render('admin_post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form->createView()
+        ]);
+    }
+
+     /**
+     * @Route("/admin/posts/create", name="admin_create")
+     */
+    public function create (Request $request) 
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+           $em = $this->getDoctrine()->getManager();
+           $em->persist($post);
+           $em->flush();
+           $this->addFlash('success', 'Post created sucessfully');
+           return $this->redirectToRoute('admin_post');
+        }
+        return $this->render('admin_post/create.html.twig', [
+            'post' => $post,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/posts/delete/{id}", name="admin_delete", methods="DELETE")
+     */
+    public function delete (Post $post, Request $request) 
+    {
+
+        if ($this->isCsrfTokenValid('delete' . $post->getId(), $request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($post);
+            $em->flush();
+         }
+        
+        $this->addFlash('success', 'Post deleted sucessfully');
+        return $this->redirectToRoute('admin_post');
+    
+    }
+
+    
+    
+}
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,20 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
+    private $repository;
+
+    public function __construct(PostRepository $repository) {
+
+        $this->repository = $repository;
+    }
     /**
      * @Route("/post/{id}", name="post")
      */
     public function index($id)
     {
-        $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
+        $post = $this->repository->find($id);
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
             'post' => $post
         ]);
     }
+
 }
