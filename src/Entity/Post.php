e741c80fe35b4559749831667050dac0c9a269ec
Bellel Oussama <oussama.bellel@etu.umontpellier.fr>
2019-11-28 17:21:11 +0100

    log

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -27,7 +27,7 @@ class Post
     private $description;
 
     /**
-     * @ORM\Column(type="datetime", options={"default"="CURRENT_TIMESTAMP"})
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
      */
     private $created_at;
 
