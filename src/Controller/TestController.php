b49f8f1792a9a7a6240068bc5e3d01d1ce38138f
Bellel Oussama <oussama.bellel@etu.umontpellier.fr>
2019-12-16 17:54:12 +0100

    pagination ok

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,16 +5,22 @@ namespace App\Controller;
 use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
+
 
 class TestController extends AbstractController
 {
     /**
      * @Route("/", name="test")
      */
-    public function index()
+    public function index(PaginatorInterface $paginator,Request $request)
     {
-        $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
-
+        // $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
+        $posts = $paginator->paginate(
+        $this->getDoctrine()->getRepository(Post::class)->findAll(),
+        $request->query->getInt('page', 1),
+        5 );
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
             'posts' => $posts
