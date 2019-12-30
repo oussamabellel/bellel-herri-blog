8c0e6712a5ebf84fd513f06e5f081c1c37625498
UNitech <bellel.oussama@live.fr>
2019-12-31 00:22:00 +0100

    post without image

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -22,7 +22,7 @@ class Post
 
     /**
      * @var string|null
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $filename;
 
